16d4044abb51972d72be90b8bc806d5c769ca28a
Kevin Grier <kevinicole1101@gmail.com>
2025-08-03 01:27:26 -0400

    remove root outputs.tf (module handles its own outputs)

--- infra/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "bucket_name" {
-  description = "The name of the GCS bucket"
-  value       = google_storage_bucket.this.name
-}
-
-output "bucket_url" {
-  description = "The GCS bucket's public or internal URL"
-  value       = "gs://${google_storage_bucket.this.name}"
-}
