8bd41d8c9f410296f45df480de63bceb7ce6ccf6
Kevin Grier <kevinicole1101@gmail.com>
2025-08-03 01:50:50 -0400

    add s3_bucket outputs and wire into root outputs

--- infra/output.tf
+++ infra/output.tf
@@ -4,6 +4,16 @@ output "gcs_bucket_name" {
 }
 
 output "gcs_bucket_url" {
-  description = "The gs:// URI of the GCS bucket"
+  description = "The GCS bucket's public or internal URL"
   value       = module.gcs_bucket.bucket_url
+}
+
+output "s3_bucket_name" {
+  description = "The name of the S3 bucket"
+  value       = module.s3_bucket.bucket_name
+}
+
+output "s3_bucket_arn" {
+  description = "The ARN of the S3 bucket"
+  value       = module.s3_bucket.bucket_arn
 }
--- modules/s3_bucket/outputs.tf
+++ modules/s3_bucket/outputs.tf
@@ -1,9 +1,9 @@
-output "bucket_name" {
+output "s3_bucket_name" {
   description = "The name of the S3 bucket"
   value       = aws_s3_bucket.this.bucket
 }
 
-output "bucket_arn" {
+output "s3_bucket_arn" {
   description = "The ARN of the S3 bucket"
   value       = aws_s3_bucket.this.arn
 }
